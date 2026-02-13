4d7a1deff61eaf4126b74a535795cfbe060462b7
Roman Pavlenko <rpavlenko.mail@gmail.com>
2026-02-13 14:49:56 +0100

    feat: modify script

--- vichy/3337875921008/scripts/script.js
+++ vichy/3337875921008/scripts/script.js
@@ -4,16 +4,23 @@
   for (let i = 0; i < accTab.length; i++) {
     accTab[i].addEventListener('click', function () {
       this.classList.add('es-active');
-      let accTabContent = this.nextElementSibling;
+      const accTabContent = this.querySelector(
+        '#es-rc #es-rc-content .es-accordion__element-content',
+      );
+      const arrow = this.querySelector(
+        '#es-rc #es-rc-content .es-acc-tab__image',
+      );
 
       const hideContent = () => {
         accTabContent.style.maxHeight = null;
         this.classList.remove('es-active');
+        arrow.classList.remove('es-active');
       };
 
       const showContent = () => {
         accTabContent.style.maxHeight = accTabContent.scrollHeight + 'px';
         this.classList.add('es-active');
+        arrow.classList.add('es-active');
       };
 
       if (accTabContent.style.maxHeight) {
